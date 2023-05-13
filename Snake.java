5ae7aa0511ffdf922b9cb3e55d1d045e3a893082
Fares <fares.selwadii@gmail.com>
2023-05-13 19:18:05 +1200

    fixed some issues and the snake now moves and created endGame screen

--- Snake.java
+++ Snake.java
@@ -4,15 +4,14 @@ import java.util.Arrays;
 public class Snake {
   private int direction;
   private int length;
-  private int[] applePosition = new int[2];
-  private ArrayList<Integer> positions = new ArrayList<>(Arrays.asList(0, 0));
+  private int[] applePosition = {13,10};
+  private ArrayList<Integer> positions = new ArrayList<>(Arrays.asList(-2, -2));
 
   public Snake() {
     this.direction = 1;
     this.length = 2;
     this.positions.addAll(Arrays.asList(4, 10, 3, 10));
-    this.applePosition[0] = 13;
-    this.applePosition[1] = 10;
+    
   }
 
   public int[] getApplePosition() {
@@ -25,9 +24,9 @@ public class Snake {
 
   public void move() {
     if (this.direction == 1) {
-      positions.addAll(2, Arrays.asList(positions.get(2) - 1, positions.get(3)));
-    } else if (this.direction == -1) {
       positions.addAll(2, Arrays.asList(positions.get(2) + 1, positions.get(3)));
+    } else if (this.direction == -1) {
+      positions.addAll(2, Arrays.asList(positions.get(2) - 1, positions.get(3)));
     } else if (this.direction == 2) {
       positions.addAll(2, Arrays.asList(positions.get(2), positions.get(3) + 1));
     } else if (this.direction == -2) {
@@ -35,9 +34,6 @@ public class Snake {
     }
     positions.remove(positions.size() - 1);
     positions.remove(positions.size() - 1);
-    for(int i =0; i<positions.size(); i++){
-        System.out.println(positions.get(i));
-    }
   }
 
   public void increseLneght() {
--- SnakeGame.java
+++ SnakeGame.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 public class SnakeGame extends JPanel implements KeyListener, ActionListener {
   private int originX = 50;
@@ -13,8 +14,9 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
   private Snake snake = new Snake();
   private Timer timer;
   private int delay = 100;
+  private JLabel endGame;
 
-  public SnakeGame(){
+  public SnakeGame() {
     setFocusable(true);
     addKeyListener(this);
     setPreferredSize(new Dimension(800, 800));
@@ -28,6 +30,20 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
+
+    if (gameEnds()) {
+      String youWon = "You Won!";
+      g.setFont(new Font("Verdana",1,80));
+      int textWidth = g.getFontMetrics().stringWidth(youWon);
+      int textHeight = g.getFontMetrics().getHeight();
+      int x = (getWidth() - textWidth)/2;
+      int y = (getHeight() - textHeight)/2 + 50;
+      System.out.println(x +""+ y);
+      g.setColor(Color.gray);
+      g.fillRect((700 - (textWidth + textWidth/4))/2 + 50,(700 - (textHeight*2))/2 + 50,textWidth + textWidth/4,textHeight*2);
+      g.setColor(Color.black);
+      g.drawString(youWon, x, y);
+    }
 
     for (int i = 0; i < numOfCells; i++) {
       g.drawLine(50, originY + sizeOfCell * i, 750, originY + sizeOfCell * i);
@@ -55,10 +71,10 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
   @Override
   public void keyPressed(KeyEvent move) {
     int keyCode = move.getKeyCode();
-    if (keyCode == KeyEvent.VK_UP && snake.getDirection() != -2) {
-      snake.changeDirection(2);
-    } else if (keyCode == KeyEvent.VK_DOWN && snake.getDirection() != 2) {
+    if (keyCode == KeyEvent.VK_UP && snake.getDirection() != 2) {
       snake.changeDirection(-2);
+    } else if (keyCode == KeyEvent.VK_DOWN && snake.getDirection() != -2) {
+      snake.changeDirection(2);
     } else if (keyCode == KeyEvent.VK_LEFT && snake.getDirection() != 1) {
       snake.changeDirection(-1);
     } else if (keyCode == KeyEvent.VK_RIGHT && snake.getDirection() != -1) {
@@ -68,6 +84,7 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
 
   public static void main(String[] args) {
     JFrame frame = new JFrame("Big Snake Good");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     SnakeGame board = new SnakeGame();
     board.setBackground(Color.green);
     frame.add(board);
@@ -78,20 +95,23 @@ public class SnakeGame extends JPanel implements KeyListener, ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    // TODO Auto-generated method stub
     snake.move();
-    
+    this.paintComponent(getGraphics());
+    if (gameEnds()) {
+      timer.stop();
+    }
+  }
+
+  private boolean gameEnds() {
+    return true;
   }
 
   @Override
   public void keyTyped(KeyEvent e) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
   }
 
   @Override
   public void keyReleased(KeyEvent e) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
   }
+
 }
